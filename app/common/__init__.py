6107963fccc295e8a5987d0c950aa1f9e2d486fe
Adriano Canofre <adriano.canofre@tvglobo.com.br>
2019-03-29 10:26:20 -0300

    [:hammer: refactor] schema in swagger

--- app/common/__init__.py
+++ app/common/__init__.py
@@ -1,4 +1,3 @@
-from .helpers import ValidateInput, check_json, check_exceptions, log_request
+from .helpers import ValidateInput, check_json, check_exceptions
 from .helpers import last_tag, last_commit, last_commit_datetime, build_working_response
 from .validate_input import POST_INPUT_SCHEMA
-from .swagger_schema import SPECS_DICT
