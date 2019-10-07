ce304332098ac34d4a1dce7956c06d7da8ad2c77
NinoCosmic <nikola.dokoski@cosmicdevelopment.com>
2019-10-07 14:24:57 +0200

    Updated some documentation.

--- app/__init__.py
+++ app/__init__.py
@@ -26,18 +26,10 @@ swagger = Swagger(app)
 #versions ends up being a list of versions as such:  (check services/__init__.py)
 #[app.services.v1, app.services.v2, ...]
 #For each one of these, the __name__() method returns this string, so we get the name by splitting this by '.' and taking the last substring
-#Each version needs to define a get_services() method which returns these data: 
+#Each version needs to define a services list which returns a list of all the APIs to be registered.
+#In order for these apis to have their endpoints added, they need to define an `endpoint` variable. For instance, `ServiceApiv3.endpoint` returns 'service'. 
+#If these classes inherit the base classes in services/*.py, then this is already handled. 
 
-#def get_services() : 
-#    return [
-#        {'api' : ServiceApiV1, 'endpoint' : 'service'}, 
-#        {'api' : HealthApiV1, 'endpoint' : 'healthcheck'}, 
-#        {'api' : WorkApiV1, 'endpoint' : 'working'}, 
-#        {'api' : InfoApiV1, 'endpoint' : 'info'}
-#    ]
-
-#Where *ApiV1 should be defined / imported and have all important methods. Then we can add the resources as : 
-#/api/v1/endpoint/
 for version in versions:
     version_name = version.__name__.split('.')[-1]
     for service in version.services:
--- app/services/v3/__init__.py
+++ app/services/v3/__init__.py
@@ -3,5 +3,4 @@ from .healthcheck import HealthApiV3
 from .working import WorkApiV3
 from .info import InfoApiV3
 
-services = [ServiceApiV3, HealthApiV3, WorkApiV3, InfoApiV3]
-
+services = [ServiceApiV3, HealthApiV3, WorkApiV3, InfoApiV3,]
